5b06f4fedf1587b0ea9a94721d1c5c3f6038d06a
Familia Sierra Cardona <fliasierra@MacBook-Air-de-Familia.local>
2020-06-07 19:32:01 -0500

    Se agregan archivos iniciales del proyecto

---
+++ funciones.rkt
@@ -0,0 +1,17 @@
+(define (num n e f)
+ (if (= n e) f (num n (+ e 1) (+ f 1))) )
+(num (char->integer #\3) 49 1)
+
+(char->integer #\3)
+
+
+
+(define (num2 n e)
+  (if (> n 1)
+(num2 (- n 1) (+ e 1))
+(integer->char e)
+
+      ))(num2 3 49)
+
+
+
